38ceeca3b2d794eea1098ea248930828a1d84770
VincentTuannn <147407930+VincentTuannn@users.noreply.github.com>
2023-11-06 19:48:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ const forgotLink=document.querySelector('.ForgotPass-link');
 const btnPopup=document.querySelector('.btnLogin-popup');
 const iconclose=document.querySelector('.icon-close');
 
-
+//Bấm vào để chuyển qua trang đăng ký
 registerLink.addEventListener('click',()=>{
     wrapper.classList.add('active');
 });
 
+//Bấm vào để chuyển qua trang đổi mật khẩu
 forgotLink.addEventListener('click',()=>{
     wrapper.classList.add('active');
 });
 
+//Bấm vào để chuyển qua trang đăng nhập
 loginLink.addEventListener('click',()=>{
     wrapper.classList.add('active');
 });
 
+//Chuyển đổi các trang
 btnPopup.addEventListener('click',()=>{
     wrapper.classList.add('active-popup');
 });
 
+//Bấm vào để tắt trang đăng nhập
 iconClose.addEventListener('click',()=>{
     wrapper.classList.remove('active-popup');
 });
