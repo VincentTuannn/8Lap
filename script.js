0bf758233c809c3ae4bf9074f7676437124a163f
VincentTuannn <147407930+VincentTuannn@users.noreply.github.com>
2023-11-19 23:34:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,59 +31,123 @@ iconclose.addEventListener('click',()=>{
     wrapper.classList.remove('active-popup');
 });
 
+
+
+
+
+
+
 //localStorage cho trang đăng ký lưu trữ thông tin
 
-function signup() {
-    // Lấy dữ liệu người dùng từ form
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-   
+function signup() 
+{
+    // Lấy email từ input
+    var email = document.getElementById('email').value;
+  
+    // Lấy mật khẩu từ input
+    var password = document.getElementById('password').value;
 
-
-    // Kiểm tra dữ liệu người dùng
-    if (email === "" || password === "") {
-        alert("Vui lòng nhập đầy đủ thông tin");
-        return;
-    }
-
-    // Chuyển đổi dữ liệu người dùng thành JSON
-    const data = {
+    // Lấy xác nhận mật khẩu từ input
+    var checkpassword = document.getElementById('checkpassword').value;
+  
+      // Tạo 1 đối tượng tài khoản mới
+      var account = [{
         email: email,
         password: password,
-    };
+        checkpassword: checkpassword
+      }];
+      
+      // Kiểm tra tài khoản đã nằm trong localStorage chưa
+      var accounts = JSON.parse(localStorage.getItem('accounts')) || [];
 
-    // Lưu dữ liệu người dùng vào Local Storage
-    localStorage.setItem("data", JSON.stringify(data));  
- 
-    // Thông báo đăng ký thành công
-    alert("Đăng ký thành công");
+      //Thêm tài khoản mới vào mảng
+      accounts.push(account);
+
+      //Lưu tài khoản vào localStorage
+      localStorage.setItem('accounts', JSON.stringify(accounts));
+  
+      
+      if(password===checkpassword)
+      {
+      // Hiển thị thông báo thành công
+      alert('Tài khoản đã tạo thành công.');
+      return;
+      }
+      else
+      {
+        alert('Tài khoản đã tạo thất bại.');
+        return;
+      }
+  } 
+
+
+
+
+// Hàm giả định để lấy danh sách tài khoản từ Local Storage
+function getStoredAccounts() {
+  // Thực tế, bạn có thể lấy dữ liệu từ Local Storage bằng cách sử dụng localStorage.getItem()
+  // Ví dụ: const storedData = localStorage.getItem("accounts");
+  // Sau đó chuyển đổi chuỗi JSON thành mảng đối tượng.
+  // Ở đây, tôi sử dụng một mảng giả định để minh họa.
+  const storedData = localStorage.getItem("accounts");
+  const accounts = JSON.parse(storedData) || [];
+  return accounts;
 }
- 
+
+
+
 
 //localStorage cho trang đăng nhập truy xuất thông tin
 
 function login() {
-    // Lấy dữ liệu người dùng từ form
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  // Lấy dữ liệu người dùng từ form
+  const gmail = document.getElementById("gmail").value;
+  const firewall = document.getElementById("firewall").value;
 
-    // Kiểm tra dữ liệu người dùng
-    if (email === "" || password === "") {
-        alert("Vui lòng nhập đầy đủ thông tin");
-        return;
-    }
+  // Kiểm tra dữ liệu người dùng
+  if (gmail === "" || firewall === "") {
+      alert("Vui lòng nhập đầy đủ thông tin");
+      return;
+  }
 
-    // Lấy dữ liệu người dùng từ Local Storage
-    const storedEmail = localStorage.getItem("email");
-    const storedPassword = localStorage.getItem("password");
+  // Lấy dữ liệu người dùng từ Local Storage hoặc một nguồn dữ liệu khác
+  const accounts = getStoredAccounts();
 
-    // So sánh dữ liệu người dùng
-    if (email === storedEmail && password === storedPassword) {
-        // Đăng nhập thành công
-        alert("Đăng nhập thành công");
-    } else {
-        // Đăng nhập thất bại
-        alert("Tài khoản hoặc mật khẩu không chính xác");
-    }
+  // Kiểm tra từng tài khoản
+  let loggedIn = false;
+  for (const account of accounts) {
+      if (gmail === account.email && firewall === account.password) {
+          // Đăng nhập thành công
+          alert("Đăng nhập thành công");
+          console.log(gmail);
+          console.log(firewall);
+          loggedIn = true;
+          break; // Thoát khỏi vòng lặp khi tìm thấy tài khoản khớp
+      }
+  }
+
+  if (!loggedIn) {
+      // Đăng nhập thất bại
+      alert("Tài khoản hoặc mật khẩu không chính xác");
+  }
 }
 
+
+
+
+
+/*
+var regex = /^(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+if(regex.test(password)) {
+  console.log("Password is valid");
+} else {
+  console.log("Password is invalid");
+} */
+
+
+/* 
+function checkPassword(password) {
+ var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+ return regex.test(password);
+}
+*/
